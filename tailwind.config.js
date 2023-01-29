59ebdc7c5504c91b80988fcc27de5aaceb57db7f
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-28 21:18:18 -0800

    Added Custom Stylings
    1. Added fontFamily Sansita and Lato
    2. Chose the font colors
    3. Compile new changes in tailwind.config.js with "npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: false, 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'title': ['Sansita', 'sans-serif'],
+        'body': ['Lato', 'sans-serif']
+      },
+      colors: {
+        'pri' : '#081F4D',  // pri class for primary color
+        'sec' : '#0083FF',  // secondary color
+        'gen' : '#3B4D71',  // general text color
+        'our-bg' : '#D9E6FF', // background color
+      }
+    }, 
   },
   plugins: [],
 }
+ 
